b4ad22d35cb316c72a4117055a0a8dd1ef120705
excf28 <deee4444@gmail.com>
2023-11-22 22:27:46 +0100

    homepage cover

--- beadando/Program.cs
+++ beadando/Program.cs
@@ -19,6 +19,16 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddRoles<IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 3;
+    options.Password.RequiredUniqueChars = 0;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
